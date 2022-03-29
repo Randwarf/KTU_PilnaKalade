52ccd2fbd8735f5a70eb4f3766692c2792d9a9c0
jusrus01 <epicemail@epicmeailc.com>
2022-03-29 19:16:07 +0300

    added mock ui manager for stats controllers

---
+++ PilnaKalade/Assets/Scripts/UI/Stats/MockUIManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.UI.Stats
+{
+    public class MockUIManager : MonoBehaviour
+    {
+        public StatsController EnemyStatsController;
+        public StatsController PlayerStatsController;
+
+        // Sample values for demonstration
+        private void Start()
+        {
+            PlayerStatsController.SetMaxBarValue(100, BarType.Defense);
+            PlayerStatsController.SetMaxBarValue(100, BarType.Health);
+            PlayerStatsController.SetMaxBarValue(100, BarType.Mana);
+            PlayerStatsController.DecreaseBarValue(100000000, BarType.Mana);
+            PlayerStatsController.IncreaseBarValue(30, BarType.Mana);
+            PlayerStatsController.DecreaseBarValue(100, BarType.Defense);
+            PlayerStatsController.SpawnEffect(StatusEffectType.Mana);
+            PlayerStatsController.SpawnEffect(StatusEffectType.Health);
+            PlayerStatsController.SpawnEffect(StatusEffectType.Mana);
+
+            EnemyStatsController.SetMaxBarValue(100, BarType.Defense);
+            EnemyStatsController.SetMaxBarValue(100, BarType.Health);
+            EnemyStatsController.SetMaxBarValue(100, BarType.Mana);
+            EnemyStatsController.DecreaseBarValue(66, BarType.Defense);
+            EnemyStatsController.SpawnEffect(StatusEffectType.Mana);
+        }
+    }
+}
