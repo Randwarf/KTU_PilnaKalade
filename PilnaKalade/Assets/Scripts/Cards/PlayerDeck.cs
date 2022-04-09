b4ecdb9201b495fa014da688d4923dd86a506e76
FreeSpirit18 <99819303+FreeSpirit18@users.noreply.github.com>
2022-04-09 16:01:20 +0300

    Added deck

---
+++ PilnaKalade/Assets/Scripts/Cards/PlayerDeck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeck : MonoBehaviour
+{
+    private static List<CardData> deck = new List<CardData>();
+    private int x;
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0;
+        CardDatabase.LoadCards();
+        for (int i = 0; i < 20; i++)
+        {
+            x = Random.Range(0,4);
+            deck[i] = CardDatabase.GetCard(x);
+        }
+    }
+    public static List<CardData> GetDeck()
+    {
+        return deck;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
